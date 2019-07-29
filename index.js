c01f5fd6644bbafdffb965db33b6de7e28b639df
vgougeon <vincent.gougeon@ynov.com>
2019-07-29 14:47:02 +0200

    Deploy route

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 const routes = require("./src/routes/routes")
 app.use(routes)
 
+// DEPLOY
+app.get('/deploy', function (req, res) {
+  res.send('Deploiement...')
+})
+//
+
 app.listen(3000, function () {
   console.log('Server ON')
 })
