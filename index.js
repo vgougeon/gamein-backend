9a08c099be555fffc98bdeb86ef8253b6879075a
vgougeon <vincent.gougeon@ynov.com>
2019-07-29 15:10:06 +0200

    Deploy testing

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(routes)
 // DEPLOY
 app.get('/deploy', function (req, res) {
   res.send('Deploiement...')
+
+  const exec = require('child_process').exec;
+  const repo = '/var/www/gamein-front-reactjs/';
+
+  exec('cd ' + repo + ' && touch test');
+  
 })
 //
 
