255ebb040827e6a9e9ed1df6a86b7e50475eb480
VincentG <vincent.gougeon@ynov.com>
2019-07-31 01:45:41 +0200

    Deploy

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 
-const mysql = require('mysql')
-const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'gamein'
-});
+// const mysql = require('mysql')
+// const db = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'gamein'
+// });
 
 const routes = require('./src/routes/routes')
 app.use(routes)
