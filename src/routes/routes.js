a548691a88453b51b71046307fad750f37876b6d
vgougeon <vincent.gougeon@ynov.com>
2019-07-31 15:52:54 +0200

    /me converted

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,18 +4,18 @@ module.exports = function(pool) {
 
   router.get('/me', async (req, res) => {
     //créer une classe user et un fichier des fichiers séparés pour les routes
-    req.session.uid = '7'
+    req.session.uid = '6'
     const [{0: user}] = await pool.execute(`
-      SELECT accounts.*, grades.name gradeName FROM accounts 
+      SELECT accounts.*, grades.name gradeName,
+      (SELECT COUNT(id) FROM posts WHERE owner = ${req.session.uid}) as posts,
+      (SELECT COUNT(id) FROM follows WHERE followed_id = ${req.session.uid}) as followers
+      FROM accounts 
       INNER JOIN grades ON accounts.grade = grades.id 
       WHERE accounts.id = ${req.session.uid}
-    `);
+    `)
     delete user.password
-    user.level = user.experience / 10 - user.experience % 10 / 10;
-    user.posts = 0;
-    user.username = req.sessionID;
-    user.followers = 0;
-
+    user.level = user.experience / 10 - user.experience % 10 / 10
+    user.username = req.sessionID
     res.send(user)
   })
 
