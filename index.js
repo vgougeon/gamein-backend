00062d21e3eb1bb53974e311edb731a391948ae0
VincentG <vincent.gougeon@ynov.com>
2019-07-31 02:12:18 +0200

    Test

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 
-// const mysql = require('mysql')
-// const db = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : '',
-//   database : 'gamein'
-// });
+const mysql = require('mysql')
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'gamein'
+});
 
+db.connect();
+db.query('SELECT username FROM accounts WHERE id = 7', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].username);
+});
 const routes = require('./src/routes/routes')
 app.use(routes)
 
