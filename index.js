f130074a62a1a2b263ac35758a268ab26c2c455a
vgougeon <vincent.gougeon@ynov.com>
2019-07-31 11:39:35 +0200

    Mysql

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql')
 const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'azerty31',
   database : 'gamein'
 });
 
