e9a6447a45613190ce626498f4861eb042e54e83
vgougeon <vincent.gougeon@ynov.com>
2019-08-04 12:09:32 +0200

    getPosts

--- src/handlers/getPosts.js
+++ src/handlers/getPosts.js
@@ -1,25 +1,20 @@
 const pool = require('../database/db')
-const me = async function(req, res){
-  req.session.uid = '6'
-  const [{0: user}] = await pool.execute(`
-    SELECT accounts.*, grades.name gradeName,
-    (SELECT COUNT(id) FROM posts WHERE owner = ${req.session.uid}) as posts,
-    (SELECT COUNT(id) FROM follows WHERE followed_id = ${req.session.uid}) as followers
-    FROM accounts 
-    INNER JOIN grades ON accounts.grade = grades.id 
-    WHERE accounts.id = ${req.session.uid}
-  `)
-  delete user.password
-  user.level = user.experience / 10 - user.experience % 10 / 10
-  res.send(user)
-}
-
-const pool = require('../database/db')
 const getPosts = async function(req, res){
   req.session.uid = '6'
 
-  const [{0: posts}] = await pool.execute(`
-  SELECT * FROM posts
+  const [posts] = await pool.execute(`
+    SELECT posts.*, accounts.username, accounts.display_name, accounts.avatar, grades.name, like_post.account_id,
+    (select count(*) from like_post WHERE post_id = posts.id) likes,
+    (select count(*) from comments WHERE post_id = posts.id) comments,
+    p.path
+    FROM posts
+    LEFT JOIN like_post ON posts.id = like_post.post_id
+    AND like_post.account_id = " . $id . "
+    LEFT JOIN accounts ON accounts.id = posts.owner
+    LEFT JOIN grades ON grades.id = accounts.grade
+    LEFT JOIN posts_picture p ON p.post_id = posts.id
+    ORDER BY posts.date DESC
+    LIMIT 5
   `)
   res.send(posts)
 }
