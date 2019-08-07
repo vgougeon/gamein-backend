8cee9e6500e6edc648e5dd8fe70aee6647bd3478
vgougeon <vincent.gougeon@ynov.com>
2019-08-07 12:09:03 +0200

    Image compression + clean packages

--- src/handlers/newSkin.js
+++ src/handlers/newSkin.js
@@ -1,9 +1,10 @@
 const pool = require('../database/db')
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 const newSkin = async function(req, res){
     if (req.files !== undefined && Object.keys(req.files).length == 0) {
         return false
     }
-    
     const [skin] = await pool.execute(`
     INSERT INTO skins VALUES()
     `);
@@ -12,11 +13,25 @@ const newSkin = async function(req, res){
     INSERT INTO skins_media (skin_id, media_id) VALUES (${skin.insertId}, ${gameId})
     `);
 
-    req.files.skin.mv(`/var/www/assets/skin/${skin.insertId}.jpg`, function(err) {
+    req.files.skin.mv(`./${skin.insertId}.jpg`, function(err) {
         if (err)
           return res.status(500).send(err);
         res.send('File uploaded!');
     });
+
+    if(req.files.skin.size > 250000){
+        let quality = Math.floor(req.files.skin.size  * (-0.0000666) + 100)
+        if(quality >= 95) quality = 95
+        if(quality <= 30) quality = 30
+        await imagemin([`./${skin.insertId}.jpg`], {
+            destination: './test',
+            plugins: [
+                imageminMozjpeg({
+                    quality: quality
+                })
+            ]
+        });
+    }
 }
 
 module.exports = newSkin
