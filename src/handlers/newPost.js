58e9d612f779aab183aff1ceab086275ff440b30
vgougeon <vincent.gougeon@ynov.com>
2019-08-09 11:24:50 +0200

    newPost response

--- src/handlers/newPost.js
+++ src/handlers/newPost.js
@@ -26,8 +26,29 @@ const newPost = async function(req, res){
 
         compress(file, path)
     }
-    
-    res.status(200).send('success')
+
+    const [{0: newPost}] = await pool.execute(`
+    SELECT 
+    posts.*,
+    DATE_FORMAT(posts.date, "%M %e - %H:%i") as dateformat,
+    accounts.username, accounts.display_name, accounts.avatar, 
+    grades.name, 
+    like_post.account_id,
+    (select count(*) from like_post WHERE post_id = posts.id) likes,
+    (select count(*) from comments WHERE post_id = posts.id) comments,
+    p.path
+    FROM posts
+    LEFT JOIN like_post ON posts.id = like_post.post_id
+    AND like_post.account_id = " . $id . "
+    LEFT JOIN accounts ON accounts.id = posts.owner
+    LEFT JOIN grades ON grades.id = accounts.grade
+    LEFT JOIN posts_picture p ON p.post_id = posts.id
+    ORDER BY posts.date DESC
+    LIMIT 5
+    WHERE posts.id = ?`,
+    [post.insertId])
+
+    res.status(200).send(newPost)
 }
 
 module.exports = newPost
