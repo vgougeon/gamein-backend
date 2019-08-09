70070751f522251b2b669707f24c664beaf03f74
VincentG <vincent.gougeon@ynov.com>
2019-08-10 00:37:01 +0200

    getGames

--- src/handlers/getGames.js
+++ src/handlers/getGames.js
@@ -3,25 +3,31 @@ const getGames = async function(req, res){
   const offset = req.query.offset || 0;
 
   const [games] = await pool.execute(`
-  SELECT m.id, GROUP_CONCAT(c.name, ':', c.color) as consoles FROM media m
-  JOIN media_consoles mc ON mc.media_id = m.id
-  JOIN consoles c ON mc.console_id = c.id
+  SELECT m.id, m.name, year(m.release_date) as release_year,
+  IF( 
+    c.id IS NULL, 
+    json_array(),
+    JSON_ARRAYAGG(JSON_OBJECT(
+      "id", c.id,
+      "name", c.name,
+      "short", c.short,
+      "color", c.color,
+      "background", c.background
+      ))
+  ) as console
+  FROM media m
+  LEFT JOIN media_consoles mc ON mc.media_id = m.id
+  LEFT JOIN consoles c ON mc.console_id = c.id
   GROUP BY m.id
-  LIMIT 8
-  
   `)
-
-  for(let game of games){
-    game.consoles = game.consoles.split(',')
-    for(let i = 0; i < game.consoles.length; i++){
-      game.consoles[i] = game.consoles[i].split(':')
-      game.consoles[i] = {
-        name: game.consoles[i][0],
-        color: game.consoles[i][1]
-      }
-    }
-  }
   res.send(games)
 }
 
 module.exports = getGames
+
+// GROUP_CONCAT(
+//   JSON_OBJECT(
+//     'name', c.name,
+//     'color', c.color
+//   )
+// ) AS list
