970358917588da75e74d51edd96cbb7defedd7f6
VincentG <vincent.gougeon@ynov.com>
2019-09-09 20:34:18 +0200

    Like count OST

--- src/handlers/getGameOst.js
+++ src/handlers/getGameOst.js
@@ -1,12 +1,22 @@
 const pool = require('../database/db')
+require('../services/validation')();
 const getGameOst = async function(req, res){
     if(req.query.game === undefined || isNaN(req.query.game)){   
         return res.send(500)
     }
+    let id = 0
+    const user = validation(req)
+    id = user.id || 0
     const [ost] = await pool.execute(`
-    SELECT * FROM ost WHERE media_id = ?`
-    ,[req.query.game])
-    
+    SELECT ost.*,
+    (select count(*) from like_ost WHERE ost_id = ost.id) likes,
+    like_ost.account_id liked
+    FROM ost
+    LEFT JOIN like_ost ON ost.id = like_ost.ost_id
+    AND like_ost.account_id = ?
+    WHERE ost.media_id = ?`
+    ,[id, req.query.game])
+
     res.send(ost)
 }
 
