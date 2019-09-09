4f26c5252854e398799d8cd9b8925d236757bf6b
VincentG <vincent.gougeon@ynov.com>
2019-09-09 20:30:54 +0200

    Like count on getGameOst

--- src/handlers/getConsoles.js
+++ src/handlers/getConsoles.js
@@ -1,6 +1,6 @@
 const pool = require('../database/db')
 const getConsoles = async function(req, res){
-
+  
   const [consoles] = await pool.execute(`
   SELECT id, name, background from consoles
   `)
