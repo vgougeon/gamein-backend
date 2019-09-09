31b3ac66c9bd56648f87165e6954a64c7a961ed5
VincentG <vincent.gougeon@ynov.com>
2019-09-10 01:21:39 +0200

    getOst likes and liked

--- src/handlers/getOst.js
+++ src/handlers/getOst.js
@@ -3,11 +3,18 @@ const getOst = async function(req, res){
     if(req.query.id === undefined || isNaN(req.query.id)){   
         return res.send(500)
     }
+    let id = 0
+    const user = validation(req)
+    id = user.id || 0
     const [{0: ost}] = await pool.execute(`
-    SELECT ost.*, media.name as media_name FROM ost
-    JOIN media ON media.id = ost.media_id
+    SELECT ost.*, media.name as media_name,
+    (select count(*) from like_ost WHERE ost_id = ost.id) likes
+    FROM ost
+    LEFT JOIN media ON media.id = ost.media_id
+    LEFT JOIN like_ost ON ost.id = like_ost.ost_id
+    AND like_ost.account_id = ?
     WHERE ost.id= ?`
-    ,[req.query.id])
+    ,[id, req.query.id])
     
     res.send(ost)
 }
