db8391db0618810d6e8cc46af151c065d1fe0569
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2019-12-12 12:02:18 +0100

    Express fileUpload limit

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const socket = require('./src/socket/root')(server);
 server.listen(3000)
 
 app.use(cors())
-app.use(fileUpload())
+app.use(fileUpload({
+  limits: { fileSize: 10 * 1024 * 1024 },
+}));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
