f99a9b1097c6cbe06aa89bc0967462cdbd2e5cf2
VincentG <vincent.gougeon@ynov.com>
2020-04-27 16:47:30 +0200

    Register checks + insert

--- src/handlers/register.js
+++ src/handlers/register.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const log = require('../services/logging')
 const register = async function(req, res){
   log.info("register.js", "Register attempt", req.body.params.username)
+  if((/^[a-z0-9]+$/i).test(req.body.params.username)) return res.status(403).send('only-alphanumerical')
   if(req.body.params.username.length < 3) return res.status(403).send('username-too-small')
   if(req.body.params.username.length > 15) return res.status(403).send('username-too-long')
   if(req.body.params.password.length < 4) return res.status(403).send('password-too-small')
@@ -12,7 +13,12 @@ const register = async function(req, res){
     FROM accounts 
     WHERE username = '${req.body.params.username}'
   `)
-  return res.status(200).send(user)
+  if(user) return res.status(403).send('username-not-available')
+  const pass = await bcrypt.hash(req.body.params.password, 10)
+  const [newUser] = await pool.execute(`
+    INSERT INTO accounts (username, password, display_name) VALUES (?, ?, ?)`,
+    [req.body.params.username, pass, req.body.params.username]);
+  res.status(200).send(newUser)
   // const match = await bcrypt.compare(req.body.params.password, user.password)
   // if(match){
   //   const token = jwt.sign({id: user.id, name: user.username}, 'temp_key', {
